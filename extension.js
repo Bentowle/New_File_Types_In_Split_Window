a96020bfe92d544d32995fe5ab58152cf297cc72
Bentowle <towleb86@gmail.com>
2024-01-29 17:26:10 -0500

    first upload

---
+++ extension.js
@@ -0,0 +1,33 @@
+const vscode = require('vscode');
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+	let disposableArduino = vscode.commands.registerCommand('extension.newArduinoFile', function () {
+		vscode.workspace.openTextDocument({ content: '', language: 'cpp' }).then(doc => {
+			vscode.window.showTextDocument(doc, vscode.ViewColumn.Beside);
+		});
+	});
+
+	let disposablePython = vscode.commands.registerCommand('extension.newPythonFile', function () {
+		vscode.workspace.openTextDocument({ content: '', language: 'python' }).then(doc => {
+			vscode.window.showTextDocument(doc, vscode.ViewColumn.Beside);
+		});
+	});
+
+	let disposableHtml = vscode.commands.registerCommand('extension.newHtmlFile', function () {
+		vscode.workspace.openTextDocument({ content: '', language: 'html' }).then(doc => {
+			vscode.window.showTextDocument(doc, vscode.ViewColumn.Beside);
+		});
+	});
+
+	context.subscriptions.push(disposableArduino, disposablePython, disposableHtml);
+}
+
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
